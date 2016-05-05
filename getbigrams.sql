164d52e6ca8846423c62fc7d846a8b68ad0b3954
dorinoltean <dorinoltean@gmail.com>
2016-05-05 13:42:13 +0300

    added bigram script

--- getbigrams.sql
+++ getbigrams.sql
@@ -1,10 +1,5 @@
-# bigquery-ngrams
-Use bigqeruy to split text into ngrams
-
-
-## Bigrams:
-
-*Problem: Using bigquery, split text into bigram with a single query.*
+/*
+Problem: Using bigquery, split text into bigram with a single query.
 
 To solve this problem we use regex to replace every second space with another character ( in my case I use `|` character ). Second step is to split by that character. Thrid step is to filter out single words resulted.
 
@@ -12,5 +7,19 @@ text: "we want to split text into bigrams"
 step1: regexreplace  => "we want|to split|text into|bigrams" + "|" + "we|want to|split text|into bigrams"
 step2: split => ["we want", "to split", "text into", "bigrams", "we", "want to", "split text", "into bigrams"]
 step3: filter bigrams without space =>  ["we want", "to split", "text into", "want to", "split text", "into bigrams"]
+*/
 
-*file: [getbigrams.sql]"
+SELECT bigram FROM  
+ (
+	SELECT 
+		split
+		(
+			REGEXP_REPLACE(text, '([^\\s]+\\s[^\\s]*)\\s', '\\1|') +  /*/  replaces every second space with | character /*/
+			'|'+ 
+			REGEXP_REPLACE(text, '([^\\s]+)\\s([^\\s]+\\s?)', '\\1|\\2'),  /*/ replaces first, third, fifth, etc. space with | character /*/
+			
+			'|'  /*/ splits on | character to get multi words /*/
+		) as bigram
+	FROM [tablename]
+)  t
+WHERE t.bigram contains ' '
