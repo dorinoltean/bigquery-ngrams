12c1863ffb8aa89b7ba5371c4a6f7a84688eb1b8
Rahul <contact@rahulprasad.com>
2018-06-25 02:10:22 +0530

    add support for sentence sanitization

--- getBigramsStandard.sql
+++ getBigramsStandard.sql
@@ -6,13 +6,17 @@ Problem: Using bigquery, split text into bigram with a single query.
 To solve this problem we use regex to replace every second space with another character ( in my case I use `|` character ). Second step is to split by that character. Thrid step is to filter out single words resulted.
 
 text: "we want to split text into bigrams"
-step1: regexreplace  => "we want|to split|text into|bigrams" + "|" + "we|want to|split text|into bigrams"
-step2: split => ["we want", "to split", "text into", "bigrams", "we", "want to", "split text", "into bigrams"]
-step3: filter bigrams without space =>  ["we want", "to split", "text into", "want to", "split text", "into bigrams"]
+step1: Sanitize text
+		"hello #$%^&* ` ` `world (how) are you # $ 910 %9k" ====> "hello world how are you 910 9k"
+		Trim special characters from all the words in the sentance
+step2: regexreplace  => "we want|to split|text into|bigrams" + "|" + "we|want to|split text|into bigrams"
+step3: split => ["we want", "to split", "text into", "bigrams", "we", "want to", "split text", "into bigrams"]
+step4: filter bigrams without space =>  ["we want", "to split", "text into", "want to", "split text", "into bigrams"]
 */
 
 WITH sentences AS (
-  SELECT 'hello   world how are you ' AS sentence
+            SELECT 'hello #$%^&* ` ` `world (how) are you # $ 910 %9k some.email@gmail.com incredible' AS sentence
+  UNION ALL SELECT '~भारत माता की "जय"' AS sentence
 )
 
 SELECT bigrams FROM  
@@ -27,7 +31,14 @@ SELECT bigrams FROM
       		),  
 			'|'
 		) as bigram
-	FROM sentences -- Table name
+	FROM (
+		SELECT 
+			-- Sanitize text
+			REGEXP_REPLACE(sentence , r'[\[\]\\`,~!@#$%^&*()\-_=+}{\'";:/?.>,<]*(.*?)[\[\]\\`,~!@#$%^&*()\-_=+}{\'";:/?.>,<]*?(\s+?|$)', '\\1 ') as sentence
+		FROM sentences
+	)
 )  t
 CROSS JOIN UNNEST(t.bigram) as bigrams
 WHERE REGEXP_CONTAINS(bigrams, r"[^\s]+\s+[^\s]+")
+
+
